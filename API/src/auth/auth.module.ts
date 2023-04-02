5580e6eed999e39dacbd1cb8d216abd81ed6da72
AlexandreRolland <arksfroll@gmail.com>
2023-04-02 18:40:38 +0200

    fix dot.env

--- API/src/auth/auth.module.ts
+++ API/src/auth/auth.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule } from '@nestjs/config';
 import { JwtStrategy } from './strategy/passport-jwt.stategy';
+import { AuthController } from './auth.controller';
 
 @Module({
   imports: [
