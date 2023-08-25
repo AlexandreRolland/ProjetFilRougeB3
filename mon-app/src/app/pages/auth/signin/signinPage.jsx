464aa2f1d9be1d3e9e4713cf4f2d494461f5c937
AlexandreRolland <arksfroll@gmail.com>
2023-08-25 15:12:15 +0200

    Protected route

--- mon-app/src/app/pages/auth/signin/signinPage.jsx
+++ mon-app/src/app/pages/auth/signin/signinPage.jsx
@@ -1,22 +1,10 @@
-import React, { useEffect, useContext } from "react";
-import { useNavigate } from 'react-router-dom'; // utilisez useHistory pour react-router-dom v5 ou inférieure
-import UserContext from "../../../../setup/contexts/UserContext";
+import React from "react";
+
 import SigninForm from "../../../components/form/auth/signIn/signinForm.jsx";
 import Footer from "../../../layouts/footer/footer";
 import Header from "../../../layouts/header/nav.jsx";
 
 const SigninPage = () => {
-    const { user } = useContext(UserContext);
-    const navigate = useNavigate(); // utilisez useHistory pour react-router-dom v5 ou inférieure
-
-    // Rediriger l'utilisateur s'il est déjà connecté
-    useEffect(() => {
-        if (user) {
-            navigate('/room_form'); // modifiez cela selon la page vers laquelle vous souhaitez rediriger
-        }
-    }, [user, navigate]);
-
-    console.log(user);
 
     return (
         <div className="fullPage">
