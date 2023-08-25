0b6a61272e7f050a7d68942c3fc435cf2815bee3
AlexandreRolland <arksfroll@gmail.com>
2023-08-25 16:43:50 +0200

    NavBar v4

--- mon-app/src/app/layouts/header/nav.jsx
+++ mon-app/src/app/layouts/header/nav.jsx
@@ -50,31 +50,35 @@ const Nav = () => {
             <section className="header-bg">
 
                 <div className="container">
-                    <div className="logo">
-                        <Link to="/" className="primary-color logo">IDECO</Link>
-                    </div>
-                    <div className="menu-icon" onClick={handleShowNavbar}>
-                        {/* <Hamburger /> */}
-                        <div className="square"></div>
-                    </div>
-                    <div className={`nav-elements  ${showNavbar && 'active'}`}>
-                        <ul>
-                            <li>
-                                <NavLink to="/signin">Home</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/annonce_list">Se faire conseiller</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/my_annonces">Discussions</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/signup">Blog</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to='/annonce_chat' className="button" >Contacter un expert</NavLink>
-                            </li>
-                        </ul>
+                    <div className="header">
+
+
+                        <div className="logo">
+                            <Link to="/" className="primary-color logo">IDECO</Link>
+                        </div>
+                        <div className="menu-icon" onClick={handleShowNavbar}>
+                            {/* <Hamburger /> */}
+                            <div className="square"></div>
+                        </div>
+                        <div className={`nav-elements  ${showNavbar && 'active'}`}>
+                            <ul>
+                                <li>
+                                    <NavLink to="/signin">Home</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/annonce_list">Se faire conseiller</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/my_annonces">Discussions</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/signup">Blog</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to='/annonce_chat' className="button" >Contacter un expert</NavLink>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </section>
