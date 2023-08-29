a80ba270e950b9cf123dfc0a5f414d5656b05578
AlexandreRolland <arksfroll@gmail.com>
2023-08-29 10:52:03 +0200

    User Role test path + set terminé

--- mon-app/src/setup/router/index.jsx
+++ mon-app/src/setup/router/index.jsx
@@ -24,7 +24,7 @@ const Router = () => {
         initializeUser();
     }, []);
 
-    console.log(user);
+    console.log(`Utilisateur : ${user}`);
     
     return (
         <UserContext.Provider value={{ user, setUser }}>
