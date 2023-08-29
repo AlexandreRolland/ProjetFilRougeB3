5a27856217ee773b97a493c4bb539c9abe78f3c3
AlexandreRolland <arksfroll@gmail.com>
2023-08-29 11:50:16 +0200

    Hp reassurances

--- mon-app/src/app/components/form/hp-form/hp-form.jsx
+++ mon-app/src/app/components/form/hp-form/hp-form.jsx
@@ -8,36 +8,36 @@ const HpForm = () => {
                 <div className="parent">
                     <div className="child-icone"></div>
                     <div className="child-txt">
-                        <span className="title">Test</span>
-                        <p className="content">paragraphe de test, ceci est un test</p>
+                        <span className="title">Abordable</span>
+                        <p className="content">Une solution adaptée à tous les budgets en fonction de votre pièce</p>
                     </div>
                 </div>
                 <div className="parent">
                     <div className="child-icone"></div>
                     <div className="child-txt">
-                        <span className="title">Test</span>
-                        <p className="content">paragraphe de test, ceci est un test</p>
+                        <span className="title">Expertise</span>
+                        <p className="content">Des conseils délivrés par des experts diplômés en décoration.</p>
                     </div>
                 </div>
                 <div className="parent">
                     <div className="child-icone"></div>
                     <div className="child-txt">
-                        <span className="title">Test</span>
-                        <p className="content">paragraphe de test, ceci est un test</p>
+                        <span className="title">Simplicité</span>
+                        <p className="content">Une consultation via chat pour des conseils fluides et rapides.</p>
                     </div>
                 </div>
                 <div className="parent">
                     <div className="child-icone"></div>
                     <div className="child-txt">
-                        <span className="title">Test</span>
-                        <p className="content">paragraphe de test, ceci est un test</p>
+                        <span className="title">Efficacité</span>
+                        <p className="content">Obtenez des conseils précieux en seulement 30 minutes.</p>
                     </div>
                 </div>
                 <div className="parent">
                     <div className="child-icone"></div>
                     <div className="child-txt">
-                        <span className="title">Test</span>
-                        <p className="content">paragraphe de test, ceci est un test</p>
+                        <span className="title">Personnalisation</span>
+                        <p className="content">Des recommandations sur-mesure selon vos goûts.</p>
                     </div>
                 </div>
             </div>
