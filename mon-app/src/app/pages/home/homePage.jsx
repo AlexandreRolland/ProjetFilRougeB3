cb0b1310c3e8f9a9e1b0493e6aadf2e063dac777
AlexandreRolland <arksfroll@gmail.com>
2023-08-31 14:23:42 +0200

    Articles API sur Front

--- mon-app/src/app/pages/home/homePage.jsx
+++ mon-app/src/app/pages/home/homePage.jsx
@@ -6,6 +6,7 @@ import Steps from "../../components/home/steps/steps.jsx";
 import Banner from "../../components/home/banner/banner.jsx";
 import Articles from "../../components/home/articles/articles.jsx";
 import { Link } from "react-router-dom";
+import AdvertService from "../../services/AdvertService";
 
 
 
@@ -14,19 +15,14 @@ function HomePage() {
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
-      // C'est ici que vous appelerez votre service pour obtenir les articles
-      // Simulons cela pour l'instant
-      const dummyData = [
-          { title: "Article 1 test titre assez long et compliqué car long il fait 3 lignes allo test", image: "https://www.leparisien.fr/resizer/XEtkllOP69v9JTN3zmcPCA2iFlA=/932x582/cloudfront-eu-central-1.images.arcpublishing.com/lpguideshopping/WVEBLSJVIMDGHKFBH6EW6WFXMU.jpg", summary: "Résumé 1" },
-          { title: "Article 2", image: "https://www.remi-batte.fr/wp-content/uploads/2020/10/RB-Fils-149.jpg", summary: "Résumé 2" },
-          { title: "Article 3", image: "https://www.idkrea.com/sites/idkrea.com/files/actualite/5-mots-cles_decoration-interieure_harmonie_idkrea-rennes.jpg", summary: "Résumé 3" },
-          { title: "Article 4", image: "https://maison.20minutes.fr/wp-content/uploads/2020/04/chambre-bleu-art-deco-istock.jpg", summary: "Résumé 4" },
-          { title: "Article 5", image: "https://maison.20minutes.fr/wp-content/uploads/2020/10/1-une-implantation-atypique-pour-exploiter-lespace-mobalpa.jpg", summary: "Résumé 5" },
-          // ... Ajoutez autant d'articles simulés que vous le souhaitez
-      ];
-
-      setArticles(dummyData);
-  }, []);
+    AdvertService.getAdverts()
+        .then(data => {
+            setArticles(data);
+        })
+        .catch(error => {
+            console.error("Erreur lors de la récupération des articles:", error);
+        });
+}, []);
 
   return (
     <>
