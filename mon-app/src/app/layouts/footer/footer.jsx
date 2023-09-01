8a518cfe620729d89f5434e29fbf0a4c68b03f76
AlexandreRolland <arksfroll@gmail.com>
2023-09-01 16:16:11 +0200

    Single Article responsive

--- mon-app/src/app/layouts/footer/footer.jsx
+++ mon-app/src/app/layouts/footer/footer.jsx
@@ -1,6 +1,22 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { useContext } from "react";
+import UserContext from "../../../setup/contexts/UserContext.js";
+import { TokenServices } from "../../../app/services/token.services.js";
 
 const Footer = () => {
+
+    const { user } = useContext(UserContext);
+    const history = useHistory();
+
+    const handleLogout = () => {
+        TokenServices.removeToken(); // Supprimer le token d'accès
+
+        if (setUser) {
+            setUser(null); // Mettre à jour le contexte de l'utilisateur
+        }
+
+        history.push("/signin"); // Rediriger vers la page de connexion
+    };
 
     return (
 
@@ -17,6 +33,10 @@ const Footer = () => {
                         <li><Link to="/signin">Se faire conseiller</Link></li>
                         <li><Link to="/signin">Discussions</Link></li>
                         <li><Link to="/signin">Blog</Link></li>
+                        {/* se déconnecter */}
+                        {user &&
+                            <li onClick={handleLogout}><>Se déconnecter</></li>
+                        }
                     </ul>
 
 
--- mon-app/src/app/layouts/header/nav.jsx
+++ mon-app/src/app/layouts/header/nav.jsx
@@ -2,38 +2,6 @@
 import { Link } from 'react-router-dom'
 
 
-// const Nav = () => {
-//     return (
-
-//         <nav className="header-bg">
-//             <section className="container">
-
-//                 <div className="header">
-//                     <div>
-//                         <Link to="/" className="primary-color logo">IDECO</Link>
-//                     </div>
-
-//                         <li><Link to="/signin">Home</Link></li>
-//                         <li><Link to="/annonce_list">Se faire conseiller</Link></li>
-//                         <li><Link to="/my_annonces">Discussions</Link></li>
-//                         <li><Link to="/signup">Blog</Link></li>
-
-
-//                     <div>
-//                         <Link to='/annonce_chat'  className="button" >Contacter un expert</Link>
-//                         <Link to="/signin" ></Link>
-//                     </div>
-
-
-
-//                 </div>
-//             </section>
-//         </nav>
-//     );
-// };
-
-// export default Nav;
-
 import { useContext, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import UserContext from '../../../setup/contexts/UserContext'
