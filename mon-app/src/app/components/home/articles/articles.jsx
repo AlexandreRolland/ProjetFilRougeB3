2c346ac70e6ed03536bd564b889b763085e83dfa
AlexandreRolland <arksfroll@gmail.com>
2023-09-01 15:15:45 +0200

    Single Article

--- mon-app/src/app/components/home/articles/articles.jsx
+++ mon-app/src/app/components/home/articles/articles.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 const Articles = ({ articles }) => {
@@ -7,11 +8,13 @@ const Articles = ({ articles }) => {
             <h2>Consultez les <span className="primary-color">articles</span> rédigés par nos <span className="primary-color">experts</span></h2>
             <div className="articles-hp">
                 {articles.slice(0, 5).map((article, index) => (
+                    <Link key={index} to={`/article/${article.id}`}>
                     <div key={index} className="article-block" style={{ backgroundImage: `url(${article.image})` }}>
                         <div className='bottom'>
                             <h3>{article.title}</h3>
                         </div>
                     </div>
+                    </Link>
                 ))}
                 <a href="/blog/" className="article-block see-all">
                     <span className='icone'></span>
