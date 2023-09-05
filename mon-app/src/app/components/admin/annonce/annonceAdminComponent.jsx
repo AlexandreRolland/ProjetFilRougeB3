8cc983e03c311756deee10fd0b856ed25a24b3a2
AlexandreRolland <arksfroll@gmail.com>
2023-09-05 15:16:05 +0200

    AdminPage + Components

--- mon-app/src/app/components/admin/annonce/annonceAdminComponent.jsx
+++ mon-app/src/app/components/admin/annonce/annonceAdminComponent.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { AdvertService } from '../../../services/advert.services';
+//test
 
 const AnnonceAdminComponent = () => {
     const [annonces, setAnnonces] = useState([]);
