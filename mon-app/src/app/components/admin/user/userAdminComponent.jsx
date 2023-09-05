8b5c3005eb1aac8a63d9bd0fc762f3ea430b5d56
AlexandreRolland <arksfroll@gmail.com>
2023-09-05 09:54:05 +0200

    AdminPage + Components

--- mon-app/src/app/components/admin/user/userAdminComponent.jsx
+++ mon-app/src/app/components/admin/user/userAdminComponent.jsx
@@ -4,6 +4,7 @@ const UserAdminComponent = () => {
             <h2>Utilisateurs</h2>
             <div className="admin-component-container">
                 <p>user</p>
+                <p>allo push</p>
             </div>
         </div>
     );
