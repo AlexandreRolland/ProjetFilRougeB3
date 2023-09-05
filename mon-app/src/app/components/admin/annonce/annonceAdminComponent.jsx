797471d7c732e03e6fab001b220d0555ced1e9d0
AlexandreRolland <arksfroll@gmail.com>
2023-09-05 14:15:10 +0200

    AdminPage + Components

--- mon-app/src/app/components/admin/annonce/annonceAdminComponent.jsx
+++ mon-app/src/app/components/admin/annonce/annonceAdminComponent.jsx
@@ -5,13 +5,18 @@ const AnnonceAdminComponent = () => {
     const [annonces, setAnnonces] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
     const [sortOrder, setSortOrder] = useState('desc');
-    const [statusFilter, setStatusFilter] = useState('Tous');  // Nouveau état pour le filtre par statut
+    const [statusFilter, setStatusFilter] = useState('Tous');
+    const [displayedCount, setDisplayedCount] = useState(5);
+    const [showMoreButton, setShowMoreButton] = useState(true);
 
     useEffect(() => {
         async function fetchAnnonces() {
             try {
                 const allAnnonces = await AdvertService.getAdverts();
                 setAnnonces(allAnnonces);
+                if (allAnnonces.length <= 5) {
+                    setShowMoreButton(false);
+                }
             } catch (error) {
                 console.error("Erreur lors de la récupération des annonces:", error);
             }
@@ -29,42 +34,51 @@ const AnnonceAdminComponent = () => {
         filteredAnnonces = annonces.filter(annonce => annonce.description.toLowerCase().includes(searchTerm.toLowerCase()));
     }
 
-    if (statusFilter !== 'Tous') {  // Filtrage par statut
+    if (statusFilter !== 'Tous') {
         filteredAnnonces = filteredAnnonces.filter(annonce => annonce.status === statusFilter);
     }
 
     filteredAnnonces.sort((a, b) => sortOrder === 'asc' ? new Date(a.updatedAt) - new Date(b.updatedAt) : new Date(b.updatedAt) - new Date(a.updatedAt));
 
+    const handleShowMore = () => {
+        setDisplayedCount(prevCount => prevCount + 5);
+        if (filteredAnnonces.length <= displayedCount + 5) {
+            setShowMoreButton(false);
+        }
+    }
+
     return (
         <div className="admin-annonce-section">
-            <div className="filter-menu">
-                <input type="text" placeholder="Rechercher une annonce..." onChange={e => setSearchTerm(e.target.value)} />
-                
-                <select onChange={e => setStatusFilter(e.target.value)}>  {/* Menu déroulant pour le filtre par statut */}
-                    <option value="Tous">Tous les statuts</option>
-                    <option value="En Attente">En Attente</option>
-                    <option value="En Cours">En Cours</option>
-                    <option value="Terminé">Terminé</option>
-                </select>
-
-                <button onClick={toggleSortOrder}>
-                    Trier par date de modification ({sortOrder === 'asc' ? 'Ascendant' : 'Descendant'})
-                </button>
-            </div>
+            <input 
+                type="text" 
+                placeholder="Rechercher par description..." 
+                value={searchTerm}
+                onChange={e => setSearchTerm(e.target.value)}
+            />
             
+            <select value={statusFilter} onChange={e => setStatusFilter(e.target.value)}>
+                <option value="Tous">Tous</option>
+                <option value="En Attente">En Attente</option>
+                <option value="En Cours">En Cours</option>
+                <option value="Terminé">Terminé</option>
+            </select>
+            
+            <button onClick={toggleSortOrder}>
+                Trier par date {sortOrder === 'asc' ? 'croissante' : 'décroissante'}
+            </button>
+
             <div className="annonces-list">
-                {filteredAnnonces.map(annonce => (
+                {filteredAnnonces.slice(0, displayedCount).map(annonce => (
                     <div className="annonce-item" key={annonce.id}>
-                        <h3>{annonce.roomType}</h3>
-                        <p>Surface : {annonce.roomSurface} m²</p>
-                        <p>Description : {annonce.description}</p>
-                        <p>Client : {annonce.user.username}</p>
-                        <p>Statut : {annonce.status}</p>   {/* Affiche le statut de l'annonce */}
-                        <p>Rémunération : {annonce.price} €</p>
-                        <hr />
+                        <h3>{annonce.title}</h3>
+                        <p>Description: {annonce.description}</p>
+                        <p>Status: {annonce.status}</p>
+                        <p>Dernière mise à jour: {new Date(annonce.updatedAt).toLocaleString()}</p>
                     </div>
                 ))}
             </div>
+
+            {showMoreButton && <button onClick={handleShowMore}>Afficher plus</button>}
         </div>
     );
 };
