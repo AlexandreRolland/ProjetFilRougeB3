061ca927828e0edcfd09dc9f978879f2dc663741
AlexandreRolland <arksfroll@gmail.com>
2023-09-07 15:43:41 +0200

    icones

--- mon-app/src/app/components/form/hp-form/hp-form.jsx
+++ mon-app/src/app/components/form/hp-form/hp-form.jsx
@@ -6,35 +6,35 @@ const HpForm = () => {
 
             <div className="hp-reassurance">
                 <div className="parent">
-                    <div className="child-icone"></div>
+                    <div className="child-icone abord"></div>
                     <div className="child-txt">
                         <span className="title">Abordable</span>
                         <p className="content">Une solution adaptée à tous les budgets en fonction de votre pièce</p>
                     </div>
                 </div>
                 <div className="parent">
-                    <div className="child-icone"></div>
+                    <div className="child-icone expert"></div>
                     <div className="child-txt">
                         <span className="title">Expertise</span>
                         <p className="content">Des conseils délivrés par des experts diplômés en décoration.</p>
                     </div>
                 </div>
                 <div className="parent">
-                    <div className="child-icone"></div>
+                    <div className="child-icone simple"></div>
                     <div className="child-txt">
                         <span className="title">Simplicité</span>
                         <p className="content">Une consultation via chat pour des conseils fluides et rapides.</p>
                     </div>
                 </div>
                 <div className="parent">
-                    <div className="child-icone"></div>
+                    <div className="child-icone efficace"></div>
                     <div className="child-txt">
                         <span className="title">Efficacité</span>
                         <p className="content">Obtenez des conseils précieux en seulement 30 minutes.</p>
                     </div>
                 </div>
                 <div className="parent">
-                    <div className="child-icone"></div>
+                    <div className="child-icone perso"></div>
                     <div className="child-txt">
                         <span className="title">Personnalisation</span>
                         <p className="content">Des recommandations sur-mesure selon vos goûts.</p>
