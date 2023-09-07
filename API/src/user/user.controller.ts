849557bcef487760a8b4a73fa9961ae409867ef2
AlexandreRolland <arksfroll@gmail.com>
2023-09-07 16:18:02 +0200

    solde

--- API/src/user/user.controller.ts
+++ API/src/user/user.controller.ts
@@ -18,7 +18,7 @@ export class UserController {
   }
 
   @Get(':id')
-  findOne(@Param('id', ParseIntPipe) id: number) {
+  findOneById(@Param('id', ParseIntPipe) id: number) {
     return this.userService.findOne(id);
   }
 
